d7600c587843c84f262734a5c7b49c46ab8de490
gagandeep43-data-analyst <gagandeep43465@gmail.com>
2026-08-27 12:54:07 +0530

    Add files via upload

---
+++ Python_Basics/list2.py
@@ -0,0 +1,23 @@
+items=["Milk","Bread","Egg","Rice","Milk"]
+print(items)
+
+#indexing
+print(items[2])
+print(items[-2])
+
+#slicing
+print(items[2:])
+print(items[-3:])
+
+#update
+items[1]="Butter"
+print(items)
+
+#add elements
+items.append("Suger")
+print(items)
+
+items.extend(["Soap","Oil"])
+print(items)
+
+
